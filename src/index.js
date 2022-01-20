cf0bed44d3dd8d6e22403b7abd4f70f1df579541
rvcits <webapps.rvcits@gmail.com>
2022-01-20 16:29:54 +0800

    Add some CSS styles and implement inline style

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import "./index.css"
 
 // My first component
 function App() {
+	const paraStyle = {
+		"marginButtom" : "15px"
+	};
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>My Amazing ToDo-List App</h1>
-        <p>The most simple and amazing todo-list React app.</p>
+        <p style={paraStyle} >The most simple and amazing todo-list React app.</p>
+        <ul>
+          <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+          <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+          <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+        </ul>      
       </header>
     </div>
   );
