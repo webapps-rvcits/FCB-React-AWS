064f63b45782e34be0df8f1df33436a892cfb6be
rvcits <webapps.rvcits@gmail.com>
2022-01-21 14:06:19 +0800

    Add an input box

--- src/index.js
+++ src/index.js
@@ -11,14 +11,25 @@ function Task(props) {
 }
 
 function TasksList() {
+  const taskItemsList = [
+    "Follow Edukasyon.ph on Facebook.",
+    "Follow AWS Siklab Pilipinas on Facebook.",
+    "Follow Zuitt Coding Bootcamp on Facebook.",
+    "Follow Zuitt Coding Bootcamp on Instagram.",
+    "Follow Edukasyon.ph on Instagram"
+  ];
+
+
+
   return (
-    <ul>
-      <Task taskName="Follow Edukasyon.ph on Facebook."/>
-
-      <Task taskName="Follow AWS Siklab Pilipinas on Facebook."/>
-
-      <Task taskName="Follow Zuitt Coding Bootcamp on Facebook."/>
-    </ul>
+    <div>
+      <input className="task-input" />
+      <ul>
+        {taskItemsList.map((task, index) => {
+          return <Task key={index} taskName={task} />
+        })}
+      </ul>
+    </div>
   );
 }
 
